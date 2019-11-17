934b655a7d31c27a42cc713ecc246383915070d6
leahhou <hsx_124@hotmail.com>
2019-11-17 15:33:52 +1100

    minor updates

--- FoodOrderApi/HttpServer.cs
+++ FoodOrderApi/HttpServer.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using FoodOrderApi.CustomisedApiExceptions;
 using FoodOrderApp;
 using FoodOrderApp.CustomisedDomainExceptions;
+using Newtonsoft.Json;
 
 namespace FoodOrderApi
 {
@@ -44,21 +45,7 @@ namespace FoodOrderApi
 
                 try
                 {
-                    if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/orders")
-                    {
-                        ConsoleRequestMessage(req.HttpMethod);
-    
-                        responseBody = responseSender.GetResponseBody(orderController.GetAllOrders());
-                    }
-                    else if (req.HttpMethod == "GET" && req.Url.AbsolutePath.StartsWith("/orders/"))
-                    {
-                        ConsoleRequestMessage(req.HttpMethod);
-
-                        var orderId = int.Parse(req.Url.Segments[2]);
-                        
-                        responseBody = responseSender.GetResponseBody(orderController.GetOrderById(orderId));
-                    }
-                    else if (req.HttpMethod == "POST" && req.Url.AbsolutePath == "/orders")
+                    if (req.HttpMethod == "POST" && req.Url.AbsolutePath == "/orders")
                     {
                         var order = await requestParser.GetOrder(req);
 
@@ -93,6 +80,20 @@ namespace FoodOrderApi
                         {
                             responseBody = responseSender.GetResponseBody(orderController.UpdateOrder(order));
                         }
+                    }
+                    else if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/orders")
+                    {
+                        ConsoleRequestMessage(req.HttpMethod);
+    
+                        responseBody = responseSender.GetResponseBody(orderController.GetAllOrders());
+                    }
+                    else if (req.HttpMethod == "GET" && req.Url.AbsolutePath.StartsWith("/orders/"))
+                    {
+                        ConsoleRequestMessage(req.HttpMethod);
+
+                        var orderId = int.Parse(req.Url.Segments[2]);
+                        
+                        responseBody = responseSender.GetResponseBody(orderController.GetOrderById(orderId));
                     }
                     else if (req.HttpMethod == "DELETE" && req.Url.AbsolutePath.StartsWith($"/orders/"))
                     {
