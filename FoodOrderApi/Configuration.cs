bf3e4abbec5a1383ca78df9853dd9f5ef5330f1c
leahhou <hsx_124@hotmail.com>
2019-11-22 18:20:31 +1100

    adjust Environment setting

--- FoodOrderApi/Configuration.cs
+++ FoodOrderApi/Configuration.cs
@@ -8,15 +8,13 @@ namespace FoodOrderApi
     {
         public static IConfigurationBuilder Config()
         {
-            // Environment.SetEnvironmentVariable("ENVIRONMENT","local");
             var environmentName = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "local";
 
-//            if (string.IsNullOrEmpty(environmentName)) environmentName = "local";
+            Console.WriteLine($"GetCommandLineArgs: {environmentName}");
             
-            Console.WriteLine("GetCommandLineArgs: {0}", String.Join("--", environmentName));
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true,true)
+                .AddJsonFile("appsettings.json", false, true)
 //                .AddJsonFile($"appsettings.{environmentName}.json")
                 .AddJsonFile($"appsettings.{environmentName}.json", false, true)
                 .AddEnvironmentVariables();
